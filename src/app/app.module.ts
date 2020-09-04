c8f1c3ec5fd055801b689d2b8289c801acb04368
Kelly Watermeyer <kellywatermeyer@gmail.com>
2020-09-04 13:18:54 -0700

    Add week 2 content after instructional videos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  title = 'Hero Dashboard';
+  subTitle = 'Rent a Hero';
+  isAuth: boolean;
+
+  budget = '7000000000';
+
+  heroes = [{
+    name: 'Captain Marvel',
+    rent: '2000000000'
+  }, 
+  {
+    name: 'Spiderman',
+    rent: 100000
+  }, 
+  {
+    name: 'Black Panther',
+    rent: '1000000000'
+  }, 
+  {
+    name: 'Thor',
+    rent: '2000000000'
+  }];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.isAuth = true;
+  }
+}
